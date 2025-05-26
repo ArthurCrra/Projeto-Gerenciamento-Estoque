167da22be4d0fcb5462a2c959e44995a7d7b56a0
Lucas Malfatti Rieth <79464636+lucasmalfatti@users.noreply.github.com>
2025-05-26 17:57:14 -0300

    Criacao da branch front

---
+++ frontend/frontend-estoque/src/pages/Login/index.jsx
@@ -0,0 +1,19 @@
+
+import './style.css'
+
+
+function Login() {
+
+
+  return (
+    <div className='loginForm'>
+      <form action="">
+        <h1>Gerenciador de estoque</h1>
+        <input name='email' type='text'/>
+        <input name='senha' type='text'/>
+      </form>
+    </div>
+  )
+}
+
+export default Login

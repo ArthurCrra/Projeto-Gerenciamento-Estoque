ff86722b849c7e37fe74c72fb4b4765d2e314fc6
Lucas Malfatti Rieth <79464636+lucasmalfatti@users.noreply.github.com>
2025-05-28 15:46:06 -0300

    Adicionando algumas estruturas faltantes e evolucao na pagina login

--- frontend/frontend-estoque/src/main.jsx
+++ frontend/frontend-estoque/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
 import './index.css'
 import Login from './pages/Login'
+
+import { Routes } from './routes'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Login/>
+    <Routes />
   </StrictMode>,
 )

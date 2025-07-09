10fc9cb5c106d0adf5c26269a7e2804d3a3cd82c
ArthurCrra <arthur.correia04@gmail.com>
2025-07-09 16:03:41 -0300

    sync

--- frontend/frontEstoque/vite.config.ts
+++ frontend/frontEstoque/vite.config.ts
@@ -3,12 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Permite acesso externo ao contêiner
-    port: 5173,     
-    watch: {
-      // Necessário para o HMR (Hot Module Replacement) funcionar corretamente no Docker
-      usePolling: true,
-    },
-  },
 })

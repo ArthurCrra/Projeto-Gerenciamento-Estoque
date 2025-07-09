833ce82072bc2c687c6005196adfc95be9bfd3e3
ArthurCrra <arthur.correia04@gmail.com>
2025-07-09 11:30:14 -0300

    Add dev environment in docker

--- frontend/frontEstoque/vite.config.ts
+++ frontend/frontEstoque/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Permite acesso externo ao contêiner
+    port: 5173,     
+    watch: {
+      // Necessário para o HMR (Hot Module Replacement) funcionar corretamente no Docker
+      usePolling: true,
+    },
+  },
 })
